6299915c41f9721ffcb880db6eb1f24abe5ba7a7
hassan2555 <hitman2555@gmail.com>
2025-04-30 20:16:32 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,43 @@
-const CACHE_NAME = 'dish-picker-cache';
+const CACHE_NAME = 'dish-picker-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/style.css',
   '/manifest.json',
-  '/service-worker.js',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/webfonts/fa-solid-900.woff2'
+  '/icons/icon-512x512.png'
 ];
 
+// Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Activate event
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
   );
+  self.clients.claim();
 });
 
+// Fetch event
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+        .catch(() => caches.match('/index.html'));
+    })
   );
 });
