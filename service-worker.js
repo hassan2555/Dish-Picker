544de1f5087e3afc17f6c538872630d639f4b727
hassan2555 <hitman2555@gmail.com>
2025-05-02 12:46:05 +0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,19 +5,16 @@ const urlsToCache = [
   '/manifest.json',
   '/service-worker.js',
   '/style.css',
-  '/script.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
 
-// Install event - Cache essential files
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event - Clean old caches
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -33,20 +30,10 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Fetch event - Serve cached files, if available, otherwise fetch from network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // If a cached version exists, return it, otherwise fetch from network
-      return cachedResponse || fetch(event.request).then((response) => {
-        // Update the cache after fetching
-        if (event.request.url.indexOf(self.location.origin) === 0) {
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, response.clone());
-          });
-        }
-        return response;
-      });
-    })
+    caches.match(event.request).then((cachedResponse) =>
+      cachedResponse || fetch(event.request)
+    )
   );
 });
